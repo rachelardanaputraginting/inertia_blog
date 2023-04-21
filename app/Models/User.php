723e9a0aab874fadb169dbf3d52976b2aff51cdf
rachelardanaputraginting <rachelardanaputraginting@gmail.com>
2023-04-21 10:53:39 +0700

    Menambahkan fungsi hasAnyRoles

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Traits\HasRole;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -10,7 +12,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRole;
 
     /**
      * The attributes that are mass assignable.
@@ -45,15 +47,5 @@ class User extends Authenticatable
     public function articles()
     {
         return $this->hasMany(Article::class);
-    }
-
-    public function roles()
-    {
-        return $this->belongsToMany(Role::class);
-    }
-
-    public function hasRole()
-    {
-        return $this->roles()->count() >= 1 ? true : false;
     }
 }
